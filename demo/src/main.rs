a2d928e1779a22d152246c68fc40661403d7f060
Anders Voss <anderstvoss@gmail.com>
2026-09-11 00:39:41 -0700

    Use native demo window controls

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -2,9 +2,18 @@
 
 #[cfg(target_os = "linux")]
 fn main() -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+            .with_title("virtualgamepad Demo GUI")
+            .with_decorations(true)
+            .with_minimize_button(true)
+            .with_maximize_button(true)
+            .with_close_button(true),
+        ..Default::default()
+    };
     eframe::run_native(
-        "virtualgamepad",
-        eframe::NativeOptions::default(),
+        "virtualgamepad Demo GUI",
+        native_options,
         Box::new(|_| Ok(Box::new(gui::App::default()))),
     )
 }
